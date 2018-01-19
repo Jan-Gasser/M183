2e07af913587d8fb53bdf39bc3161eb9c00e9f4c
Jan <jan.gasser@bluewin.ch>
2018-01-19 08:58:02 +0100

    Aufgabe 1: File Data Access

---
+++ Pruefung_Praktisch_Musterloesung/Controllers/Lab1Controller.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Collections;
+using System.Web.Mvc;
+using System.Linq;
+
+namespace Pruefung_Praktisch_Musterloesung.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        /**
+         * Variante 1: Files mit anderem Namen aufrufen, indem man den 'type' parameter abändert
+         * URL: Meinseite.com/Lab1/Detail?type=../../&file=geheimesFiles.txt
+         * Beschreibung: Der Code nimmt den Pfad aus dem Type-Paramter und browsd durch das Fileverzeichnis.
+         * So kann der Angreiffer alle Dateien ansehen, auf die die Applikation Berechtigung hat. 
+         * Variante 2: a
+         * */
+
+
+        public ActionResult Index()
+        {
+            var type = Request.QueryString["type"];
+
+            if (string.IsNullOrEmpty(type))
+            {
+                type = "lions";
+            }
+            else
+            {
+                type.Replace("\\", "");
+                type.Replace("/", "");
+            }
+
+            var path = "~/Content/images/" + type;
+
+            List<List<string>> fileUriList = new List<List<string>>();
+
+            if (Directory.Exists(Server.MapPath(path)))
+            {
+                var scheme = Request.Url.Scheme; 
+                var host = Request.Url.Host; 
+                var port = Request.Url.Port;
+                
+                string[] fileEntries = Directory.GetFiles(Server.MapPath(path));
+                foreach (var filepath in fileEntries)
+                {
+                    var filename = Path.GetFileName(filepath);
+                    var imageuri = scheme + "://" + host + ":" + port + path.Replace("~", "") + "/" + filename;
+
+                    var urilistelement = new List<string>();
+                    urilistelement.Add(filename);
+                    urilistelement.Add(imageuri);
+                    urilistelement.Add(type);
+
+                    fileUriList.Add(urilistelement);
+                }
+            }
+            
+            return View(fileUriList);
+        }
+
+        public ActionResult Detail()
+        {
+            var file = Request.QueryString["file"];
+            var type = Request.QueryString["type"];
+
+            if (string.IsNullOrEmpty(file))
+            {
+                file = "Lion1.jpg";
+            }
+            if (string.IsNullOrEmpty(type))
+            {
+                file = "lions";
+            }
+
+            var relpath = "~/Content/images/" + type + "/" + file;
+
+            List<List<string>> fileUriItem = new List<List<string>>();
+            var path = Server.MapPath(relpath);
+
+            if (System.IO.File.Exists(path))
+            {
+                var scheme = Request.Url.Scheme;
+                var host = Request.Url.Host;
+                var port = Request.Url.Port;
+                var absolutepath = Request.Url.AbsolutePath;
+
+                var filename = Path.GetFileName(file);
+                var imageuri = scheme + "://" + host + ":" + port + "/Content/images/" + type + "/" + filename;
+
+                var urilistelement = new List<string>();
+                urilistelement.Add(filename);
+                urilistelement.Add(imageuri);
+                urilistelement.Add(type);
+
+                fileUriItem.Add(urilistelement);
+            }
+            
+            return View(fileUriItem);
+        }
+    }
+}
